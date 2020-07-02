e458dbdbb762d64b182aac9ee942fda978ac8bd1
lioent <59694379+lioent@users.noreply.github.com>
2020-07-02 18:17:19 -0300

    Environment settings fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 if (process.env.NODE_ENV == 'production') {
-    app.use(createProxyMiddleware("/api", { target: "https://protobot-backend.herokuapp.com" }));
+    module.exports = (app) => {
+        app.use(createProxyMiddleware("/api", { target: "https://protobot-backend.herokuapp.com" }));
+    };
 }
-else if(process.env.NODE_ENV == 'development') {
+else {
     module.exports = (app) => {
         app.use(createProxyMiddleware("/api", { target: "http://localhost:5000" }));
     };
