69182ae2281a93f98907489cdf280258effbc85b
Marcos Bueno <marcos@mplb.com.br>
2020-07-02 21:08:18 -0300

    Testing deploy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-if (process.env.NODE_ENV == 'production') {
+if (process.env.NODE_ENV === 'production') {
     module.exports = (app) => {
         app.use(createProxyMiddleware("/api", { target: "https://protobot-backend.herokuapp.com" }));
     };
